9956cfc3c6f96595d83ff1193abe1ed8a9e42ec4
Felix Wolff <felix.wolff2@student.hpi.de>
2017-01-23 17:26:03 +0100

    Remove uppercase

--- STREAM/run-linux-benchmark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ./benchmark-configuration.sh
-EXECUTABLE="./stream_c.exe"
-
-for config in "${CONFIGURATIONS[@]}"; do
-  cgexec -g memory:limitedrss/limit$config "$EXECUTABLE" | grep --after-context=3 'Copy:' | sed -r s/^/"$config "/g | sed s/://g | sed -r s/\\s+/,/g
-done
